f9b94a0fc7f851be1a5b2518d148f5766f03729e
fzaninotto <fzaninotto@gmail.com>
2025-07-18 14:35:24 +0200

    Improve navigation UI

--- client/components/Screen.jsx
+++ client/components/Screen.jsx
@@ -1,3 +1,4 @@
+import { ChevronLeft, ChevronRight } from "react-feather"
 import WhiteboardOutput from "./whiteboard/WhiteboardOutput";
 import { useWhiteboardState } from "./whiteboard/useWhiteboardState";
 import { ActivityIndicator } from "./ActivityIndicator";
@@ -42,14 +43,14 @@ export default function Screen({
               <div className="flex justify-between mb-2">
                 {
                   goBack &&
-                  <button className="bg-gray-800 mb-2 w-fit text-white rounded-full py-2 px-4 hover:opacity-90" type="button" onClick={goBack}>
-                    &lt; Go back
+                  <button className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80 cursor-pointer size-9" type="button" onClick={goBack}>
+                    <ChevronLeft />
                   </button>
                 }
                 {
                   goToNextPage &&
-                  <button className="bg-gray-800 mb-2 ml-auto w-fit text-white rounded-full py-2 px-4 hover:opacity-90" type="button" onClick={goToNextPage}>
-                    &gt; Next page
+                  <button className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80 cursor-pointer size-9" type="button" onClick={goToNextPage}>
+                    <ChevronRight />
                   </button>
                 }
               </div>
